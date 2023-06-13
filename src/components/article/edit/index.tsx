eafda9c2f671d71e84014b232d004c4cc367f20c
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-06-13 23:34:11 +0200

    fea(Edit): introduce

---
+++ src/components/article/edit/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { BsFillPencilFill } from 'react-icons/bs';
+import type { FC } from 'react';
+
+const BASE_EditURL = 'https://github.com/belgiumJS/belgiumjs.github.io/edit/main/content';
+
+type Props = {
+    path: string;
+};
+
+const Edit: FC<Props> = ({ path }) => (
+    <Link
+        href={`${BASE_EditURL}/${path}.md`}
+        passHref
+    >
+    Edit this page <BsFillPencilFill />
+    </Link>
+);
+
+export default Edit;
