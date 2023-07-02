c0b1455569c7d948baad9a2cc75229e61f45bf7f
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-07-02 23:29:09 +0200

    feat(style): design 404

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,6 +1,14 @@
 // it's doesn't work with next.js static generation
+import LocalizedLink from '@/components/i18n/localizedLink';
+import styles from '@/styles/app/not-found.module.scss';
 import type { FC } from 'react';
 
-const NotFound: FC = () => <div>404</div>;
+const NotFound: FC = () => (
+    <div className={styles.notFound}>
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <LocalizedLink href="/">Go to homepage</LocalizedLink>
+    </div>
+);
 
 export default NotFound;
